92c4a95ba1afe3c5d6cef5b5210381d3b4828c54
PlusIvan <ivan@ivanmoroz.net>
2020-01-31 20:33:34 +0000

    -

--- BlackJack/forms/Settings.Designer.cs
+++ BlackJack/forms/Settings.Designer.cs
@@ -35,12 +35,12 @@
             this.solo_setting_joker = new MetroSet_UI.Controls.MetroSetSwitch();
             this.metroSetLabel1 = new MetroSet_UI.Controls.MetroSetLabel();
             this.metroSetTabPage2 = new MetroSet_UI.Child.MetroSetTabPage();
+            this.metroSetLabel2 = new MetroSet_UI.Controls.MetroSetLabel();
+            this.btn_loadPATH = new MetroSet_UI.Controls.MetroSetButton();
+            this.metroSetTextBox2 = new MetroSet_UI.Controls.MetroSetTextBox();
             this.metroSetTabPage3 = new MetroSet_UI.Child.MetroSetTabPage();
             this.metroSetTextBox1 = new MetroSet_UI.Controls.MetroSetTextBox();
             this.metroSetButton1 = new MetroSet_UI.Controls.MetroSetButton();
-            this.metroSetTextBox2 = new MetroSet_UI.Controls.MetroSetTextBox();
-            this.btn_loadPATH = new MetroSet_UI.Controls.MetroSetButton();
-            this.metroSetLabel2 = new MetroSet_UI.Controls.MetroSetLabel();
             this.metroSetTabControl1.SuspendLayout();
             this.metroSetTabPage4.SuspendLayout();
             this.metroSetTabPage2.SuspendLayout();
@@ -99,12 +99,12 @@
             this.metroSetTabControl1.Controls.Add(this.metroSetTabPage4);
             this.metroSetTabControl1.Controls.Add(this.metroSetTabPage2);
             this.metroSetTabControl1.Controls.Add(this.metroSetTabPage3);
-            this.metroSetTabControl1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.metroSetTabControl1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.metroSetTabControl1.Dock = System.Windows.Forms.DockStyle.Top;
             this.metroSetTabControl1.ItemSize = new System.Drawing.Size(100, 38);
             this.metroSetTabControl1.Location = new System.Drawing.Point(0, 25);
             this.metroSetTabControl1.Name = "metroSetTabControl1";
-            this.metroSetTabControl1.SelectedIndex = 1;
+            this.metroSetTabControl1.SelectedIndex = 0;
             this.metroSetTabControl1.Size = new System.Drawing.Size(792, 380);
             this.metroSetTabControl1.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
             this.metroSetTabControl1.Speed = 20;
@@ -190,6 +190,75 @@
             this.metroSetTabPage2.ThemeName = "MetroLite";
             this.metroSetTabPage2.ToolTipText = null;
             // 
+            // metroSetLabel2
+            // 
+            this.metroSetLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.metroSetLabel2.Location = new System.Drawing.Point(3, 10);
+            this.metroSetLabel2.Name = "metroSetLabel2";
+            this.metroSetLabel2.Size = new System.Drawing.Size(156, 25);
+            this.metroSetLabel2.Style = MetroSet_UI.Design.Style.Light;
+            this.metroSetLabel2.StyleManager = null;
+            this.metroSetLabel2.TabIndex = 2;
+            this.metroSetLabel2.Text = "Track Folder";
+            this.metroSetLabel2.ThemeAuthor = "Narwin";
+            this.metroSetLabel2.ThemeName = "MetroLite";
+            // 
+            // btn_loadPATH
+            // 
+            this.btn_loadPATH.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_loadPATH.DisabledBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(65)))), ((int)(((byte)(177)))), ((int)(((byte)(225)))));
+            this.btn_loadPATH.DisabledBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(65)))), ((int)(((byte)(177)))), ((int)(((byte)(225)))));
+            this.btn_loadPATH.DisabledForeColor = System.Drawing.Color.Gray;
+            this.btn_loadPATH.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.btn_loadPATH.HoverBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(95)))), ((int)(((byte)(207)))), ((int)(((byte)(255)))));
+            this.btn_loadPATH.HoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(95)))), ((int)(((byte)(207)))), ((int)(((byte)(255)))));
+            this.btn_loadPATH.HoverTextColor = System.Drawing.Color.White;
+            this.btn_loadPATH.Location = new System.Drawing.Point(3, 73);
+            this.btn_loadPATH.Name = "btn_loadPATH";
+            this.btn_loadPATH.NormalBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(177)))), ((int)(((byte)(225)))));
+            this.btn_loadPATH.NormalColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(177)))), ((int)(((byte)(225)))));
+            this.btn_loadPATH.NormalTextColor = System.Drawing.Color.White;
+            this.btn_loadPATH.PressBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(147)))), ((int)(((byte)(195)))));
+            this.btn_loadPATH.PressColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(147)))), ((int)(((byte)(195)))));
+            this.btn_loadPATH.PressTextColor = System.Drawing.Color.White;
+            this.btn_loadPATH.Size = new System.Drawing.Size(134, 30);
+            this.btn_loadPATH.Style = MetroSet_UI.Design.Style.Light;
+            this.btn_loadPATH.StyleManager = null;
+            this.btn_loadPATH.TabIndex = 1;
+            this.btn_loadPATH.Text = "Load Track";
+            this.btn_loadPATH.ThemeAuthor = "Narwin";
+            this.btn_loadPATH.ThemeName = "MetroLite";
+            // 
+            // metroSetTextBox2
+            // 
+            this.metroSetTextBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.metroSetTextBox2.AutoCompleteCustomSource = null;
+            this.metroSetTextBox2.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.None;
+            this.metroSetTextBox2.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.None;
+            this.metroSetTextBox2.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(155)))), ((int)(((byte)(155)))));
+            this.metroSetTextBox2.DisabledBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(204)))), ((int)(((byte)(204)))));
+            this.metroSetTextBox2.DisabledBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(155)))), ((int)(((byte)(155)))));
+            this.metroSetTextBox2.DisabledForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(136)))), ((int)(((byte)(136)))));
+            this.metroSetTextBox2.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.metroSetTextBox2.HoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.metroSetTextBox2.Image = null;
+            this.metroSetTextBox2.Lines = null;
+            this.metroSetTextBox2.Location = new System.Drawing.Point(3, 38);
+            this.metroSetTextBox2.MaxLength = 32767;
+            this.metroSetTextBox2.Multiline = false;
+            this.metroSetTextBox2.Name = "metroSetTextBox2";
+            this.metroSetTextBox2.ReadOnly = false;
+            this.metroSetTextBox2.Size = new System.Drawing.Size(778, 29);
+            this.metroSetTextBox2.Style = MetroSet_UI.Design.Style.Light;
+            this.metroSetTextBox2.StyleManager = null;
+            this.metroSetTextBox2.TabIndex = 0;
+            this.metroSetTextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.metroSetTextBox2.ThemeAuthor = "Narwin";
+            this.metroSetTextBox2.ThemeName = "MetroLite";
+            this.metroSetTextBox2.UseSystemPasswordChar = false;
+            this.metroSetTextBox2.WatermarkText = "";
+            // 
             // metroSetTabPage3
             // 
             this.metroSetTabPage3.BaseColor = System.Drawing.Color.White;
@@ -198,7 +267,7 @@
             this.metroSetTabPage3.ImageKey = null;
             this.metroSetTabPage3.Location = new System.Drawing.Point(4, 42);
             this.metroSetTabPage3.Name = "metroSetTabPage3";
-            this.metroSetTabPage3.Size = new System.Drawing.Size(965, 334);
+            this.metroSetTabPage3.Size = new System.Drawing.Size(784, 334);
             this.metroSetTabPage3.Style = MetroSet_UI.Design.Style.Light;
             this.metroSetTabPage3.StyleManager = null;
             this.metroSetTabPage3.TabIndex = 2;
@@ -261,75 +330,6 @@
             this.metroSetButton1.Text = "Appeal Change";
             this.metroSetButton1.ThemeAuthor = "Narwin";
             this.metroSetButton1.ThemeName = "MetroLite";
-            // 
-            // metroSetTextBox2
-            // 
-            this.metroSetTextBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.metroSetTextBox2.AutoCompleteCustomSource = null;
-            this.metroSetTextBox2.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.None;
-            this.metroSetTextBox2.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.None;
-            this.metroSetTextBox2.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(155)))), ((int)(((byte)(155)))));
-            this.metroSetTextBox2.DisabledBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(204)))), ((int)(((byte)(204)))));
-            this.metroSetTextBox2.DisabledBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(155)))), ((int)(((byte)(155)))));
-            this.metroSetTextBox2.DisabledForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(136)))), ((int)(((byte)(136)))));
-            this.metroSetTextBox2.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this.metroSetTextBox2.HoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
-            this.metroSetTextBox2.Image = null;
-            this.metroSetTextBox2.Lines = null;
-            this.metroSetTextBox2.Location = new System.Drawing.Point(3, 38);
-            this.metroSetTextBox2.MaxLength = 32767;
-            this.metroSetTextBox2.Multiline = false;
-            this.metroSetTextBox2.Name = "metroSetTextBox2";
-            this.metroSetTextBox2.ReadOnly = false;
-            this.metroSetTextBox2.Size = new System.Drawing.Size(778, 29);
-            this.metroSetTextBox2.Style = MetroSet_UI.Design.Style.Light;
-            this.metroSetTextBox2.StyleManager = null;
-            this.metroSetTextBox2.TabIndex = 0;
-            this.metroSetTextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
-            this.metroSetTextBox2.ThemeAuthor = "Narwin";
-            this.metroSetTextBox2.ThemeName = "MetroLite";
-            this.metroSetTextBox2.UseSystemPasswordChar = false;
-            this.metroSetTextBox2.WatermarkText = "";
-            // 
-            // btn_loadPATH
-            // 
-            this.btn_loadPATH.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.btn_loadPATH.DisabledBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(65)))), ((int)(((byte)(177)))), ((int)(((byte)(225)))));
-            this.btn_loadPATH.DisabledBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(65)))), ((int)(((byte)(177)))), ((int)(((byte)(225)))));
-            this.btn_loadPATH.DisabledForeColor = System.Drawing.Color.Gray;
-            this.btn_loadPATH.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this.btn_loadPATH.HoverBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(95)))), ((int)(((byte)(207)))), ((int)(((byte)(255)))));
-            this.btn_loadPATH.HoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(95)))), ((int)(((byte)(207)))), ((int)(((byte)(255)))));
-            this.btn_loadPATH.HoverTextColor = System.Drawing.Color.White;
-            this.btn_loadPATH.Location = new System.Drawing.Point(3, 73);
-            this.btn_loadPATH.Name = "btn_loadPATH";
-            this.btn_loadPATH.NormalBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(177)))), ((int)(((byte)(225)))));
-            this.btn_loadPATH.NormalColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(177)))), ((int)(((byte)(225)))));
-            this.btn_loadPATH.NormalTextColor = System.Drawing.Color.White;
-            this.btn_loadPATH.PressBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(147)))), ((int)(((byte)(195)))));
-            this.btn_loadPATH.PressColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(147)))), ((int)(((byte)(195)))));
-            this.btn_loadPATH.PressTextColor = System.Drawing.Color.White;
-            this.btn_loadPATH.Size = new System.Drawing.Size(134, 30);
-            this.btn_loadPATH.Style = MetroSet_UI.Design.Style.Light;
-            this.btn_loadPATH.StyleManager = null;
-            this.btn_loadPATH.TabIndex = 1;
-            this.btn_loadPATH.Text = "Load Track";
-            this.btn_loadPATH.ThemeAuthor = "Narwin";
-            this.btn_loadPATH.ThemeName = "MetroLite";
-            // 
-            // metroSetLabel2
-            // 
-            this.metroSetLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.metroSetLabel2.Location = new System.Drawing.Point(3, 10);
-            this.metroSetLabel2.Name = "metroSetLabel2";
-            this.metroSetLabel2.Size = new System.Drawing.Size(156, 25);
-            this.metroSetLabel2.Style = MetroSet_UI.Design.Style.Light;
-            this.metroSetLabel2.StyleManager = null;
-            this.metroSetLabel2.TabIndex = 2;
-            this.metroSetLabel2.Text = "Track Folder";
-            this.metroSetLabel2.ThemeAuthor = "Narwin";
-            this.metroSetLabel2.ThemeName = "MetroLite";
             // 
             // Settings
             // 
