2de3174a7e1e4720db95d77a1dcb5976afb73583
PlusIvan <ivan@ivanmoroz.net>
2020-02-17 00:54:42 +0000

    publish

--- BlackJack_ReMaster/Properties/AssemblyInfo.cs
+++ BlackJack_ReMaster/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BlackJack_ReMaster")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("By Ivan Moroz")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BlackJack_ReMaster")]
